b2617bfb2cc898338eb8b7aec06efb2b5bb9aa27
Pau Fracés <pfraces@gmail.com>
2018-10-21 04:00:16 +0200

    added pretty formatter

--- src/formatters.js
+++ src/formatters.js
@@ -1,7 +1,16 @@
-const jsonFormatter = function (event) {
-  return JSON.stringify(event);
+const dateformat = require('dateformat');
+
+const jsonFormatter = function (entry) {
+  return JSON.stringify(entry);
+};
+
+const prettyFormatter = function ({ timestamp, level, message }) {
+  const date = dateformat(timestamp, 'dd/mm/yyyy HH:MM:ss');
+  return `[${date}|${level}] ${message}`;
 };
 
 module.exports = {
-  json: jsonFormatter
+  json: jsonFormatter,
+  pretty: prettyFormatter
 };
+
